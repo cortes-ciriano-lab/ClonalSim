e802ee92112f7a20d5acc2ec91a14ca629a61e57
Maria Kalyva <mkalyva@ebi.ac.uk>
2023-03-16 12:58:32 +0000

    Update coloSim.py

--- coloSim.py
+++ coloSim.py
@@ -10,12 +10,13 @@ import scipy.stats as stats
 ####################################################
 
 
-class Cell:
-    def __init__(self, cell_type):
-        self.type = cell_type
+# class Cell:
+#     def __init__(self, cell_type, position):
+#         self.type = cell_type
+#         self.position = position
 
-    def __str__(self):
-        return f"Cell type: {self.type}"
+#     def __str__(self):
+#         return f"Cell type: {self.type}"
 
 
 class Population:
@@ -43,28 +44,78 @@ class Population:
             cancer_p = (1 + s) * n / (N + n * s)
             offspring = np.random.binomial(n=1, p=cancer_p, size=N)
             self.generation_data.append(offspring)
-            # to do
+            
 
-            #1 need to check the generation_data object
-            #2 write a function to run the simulation
-            # add cell class in the code
-            # return 
+class PhyTree:
+
+    def __init__(self):
+        self.graph = {}
+        self.node_labels = {}
+
+    def connect_random_cells(self, generation_data):
+        """
+        Connects random cells of the generations in generation_data.
+        """
+        num_generations = len(p1.generation_data)
+        for i in range(num_generations - 1):
+            curr_gen = p1.generation_data[i]
+            next_gen = p1.generation_data[i + 1]
+            for j in range(p1.N):
+                if curr_gen[j] == 1:
+                    # connect to a random cell in the next generation
+                    next_gen_indices = np.argwhere(next_gen == 1).flatten()
+
+                    #!# pseudo - googled code #!# 
+                    if len(next_gen_indices) > 0:
+                        next_gen_index = np.random.choice(next_gen_indices)
+                        node1 = f"{i}_{j}"
+                        node2 = f"{i+1}_{next_gen_index}"
+                        self.add_edge(node1, node2)
+
+    def assign_edge_lengths(self, mu):
+        """
+        Assigns a length in that edge between the nodes based on a Poisson distribution with mean rate μ.
+        """
+        for edge in self.graph.edges():
+            length = np.random.poisson(mu)
+            self.graph.edges[edge]["length"] = length
+
+
+# ------------- Run Simulation ------------- #
+
+# to add: for loop for multiple iterations & all simulations in list
+
+def simulate_population_and_tree(N, generations, mut_samples, s, mu):
+    pop = Population(N, generations, mut_samples, s)
+    pop.simulate_population({})
+    tree = PhyTree()
+    tree.connect_random_cells(pop.generation_data)
+    tree.assign_edge_lengths(mu)
+    return tree
+
+
+# ------------------------------------------- #
 
 
 
-
-class PhyTree:     # add code to create genealogy (basically lines between nodes and then randomly add the same mutation number)
-    pass
-
-
-def LTT_statistics(self, synth):
+# Calculate LTT stats from simulated data
+def LTT_statistics(synth):
     """
     Calculate the LTT statistics for simulated trees.
     """
     
 
+# Distance function between observed and synthetic data
+def read_observed_data(observed):
+    """
+    Read observed tree and calculate LTT statistics and return or read in LTT statistics straight
+    """
 
-def distance_function(self, synth):
+    return LTT_stat_obs
+    
+
+# Distance function between observed and synthetic data
+def distance_function():
     """
     Calculate the distance between the observed and synthetic data.
     """
@@ -73,17 +124,20 @@ def distance_function(self, synth):
 
 
 
-def estimate_parameters(self, epsilon):
+def estimate_parameters(epsilon):
     """
     Estimate the parameters of the Wright-Fisher model with selection using the ABC algorithm.
     """
     s_prior = abcpy.Distribution(stats.uniform, 0, 2)
-    model = abcpy.Model(self.generate_synthetic_data, [self.N, s_prior])
-    data = abcpy.Data([self.obs], [self.distance_function])
-    sampler = abcpy.Sampler(model, data, epsilon=self.epsilon)
-    results = sampler.sample(self.n_samples)
+    model = abcpy.Model(generate_synthetic_data, [N, s_prior])
+    data = abcpy.Data([obs], [distance_function])
+    sampler = abcpy.Sampler(model, data, epsilon=epsilon)
+    results = sampler.sample(n_samples)
     s_estimates = results.get_parameters()[1]
     return np.mean(s_estimates)
+
+
+
 
 
 
