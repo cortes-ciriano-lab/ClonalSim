782f09c32ce810ce3368d28265f9a4f7a10afc87
Maria Kalyva <mkalyva@ebi.ac.uk>
2023-06-12 16:41:43 +0100

    UltrametricConversion importing

--- clonalSim.py
+++ clonalSim.py
@@ -333,7 +333,7 @@ def simulate_population_and_tree(N, generations, mut_samples, s, mu, output_path
     # assign random edge (branch) lengths
     phy_tree_mut = assign_edge_lengths(mu, phy_tree)
     # make tree ultrametric
-    phy_tree_ult = traverse_and_run_average(phy_tree_mut)
+    phy_tree_ult = UltrametricConversion.traverse_and_run_average(phy_tree_mut)
     # visualise tree
     #import matplotlib.patches as patches
     #plot = phy_tree_mut.draw(show_labels=False, handles=[white_patch])
@@ -351,8 +351,8 @@ def simulate_population_and_tree(N, generations, mut_samples, s, mu, output_path
 
     # normalise ltt stats
     list_of_tuples_tree = [(key, value) for key, value in ltt_gen_tree.items()]
-    data_transformed = transform_data(list_of_tuples_tree)
-    norm_data = normalise_data(data_transformed)
+    data_transformed = UltrametricConversion.transform_data(list_of_tuples_tree)
+    norm_data = UltrametricConversion.normalise_data(data_transformed)
     print("LTT Statistics Done")
 
     return phy_tree_ult , norm_data
