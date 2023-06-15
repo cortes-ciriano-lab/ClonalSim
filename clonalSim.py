5bea5c194d477bb51ad840750b2f466675782e5c
Maria Kalyva <mkalyva@ebi.ac.uk>
2023-06-15 13:29:54 +0100

    syntax correction

--- clonalSim.py
+++ clonalSim.py
@@ -364,9 +364,9 @@ while num_retries <= sim_number:
     try:
         result = simulate_population_and_tree(N=args.N, generations=args.generations, disease=args.disease,  mut_samples=args.mut_samples, s=args.s, mu=args.mu , output_path=args.output_path, num_retries=num_retries)
         num_retries += 1
-        except AssertionError:
-            num_retries += 1
-            print("AssertionError occurred, restarting simulation...")
+    except AssertionError:
+        num_retries += 1
+        print("AssertionError occurred, restarting simulation...")
         
 # call the function with the command-line arguments
 #result = simulate_population_and_tree(N=args.N, generations=args.generations, mut_samples=args.mut_samples, s=args.s, mu=args.mu)
