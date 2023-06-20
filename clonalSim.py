878712bb6abe82569e970b21473a111e0d696e4f
Maria Kalyva <mkalyva@ebi.ac.uk>
2023-06-20 18:01:04 +0100

    num retries out from out.txt

--- clonalSim.py
+++ clonalSim.py
@@ -448,7 +448,7 @@ with open(f"{args.output_path}/Simulation_results.tsv", "a", newline='') as f:
             result_tree, abc_epsilon = simulate_population_and_tree(N=args.N, generations=args.generations, disease=args.disease,  mut_samples=args.mut_samples, s=args.s, mu=args.mu , output_path=args.output_path, observed_d_path=args.observed_data_path, num_retries=num_retries)
             
             # Write all variables and args used in the file
-            f.write(f"{num_retries}\t{abc_epsilon}\t{args.N}\t{args.generations}\t{args.disease}\t{args.mut_samples}\t{args.s}\t{args.mu}\t{args.output_path}\t{args.observed_data_path}\n")
+            f.write(f"{abc_epsilon}\t{args.N}\t{args.generations}\t{args.disease}\t{args.mut_samples}\t{args.s}\t{args.mu}\t{args.output_path}\t{args.observed_data_path}\n")
             
             num_retries += 1
         except AssertionError:
